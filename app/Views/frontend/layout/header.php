468e099a6a38f339fc39fd6a6da2eda5850b0b48
muhazharrasyad <muhazharrasyad@gmail.com>
2026-04-10 00:05:23 +0700

    feat: new header

--- app/Views/frontend/layout/header.php
+++ app/Views/frontend/layout/header.php
@@ -57,6 +57,50 @@
             background-repeat: no-repeat;
             background-position: center;
         }
+        /* Kustomisasi Warna Header */
+        .main-menu .navigation > li > a {
+            color: #0052B5 !important;
+        }
+
+        .main-menu .navigation > li:hover > a,
+        .main-menu .navigation > li.current > a,
+        .sticky-header .main-menu .navigation > li:hover > a,
+        .sticky-header .main-menu .navigation > li.current > a {
+            color: #FEB605 !important;
+        }
+
+        .main-menu .navigation > li.dropdown > a:before {
+            color: #0052B5 !important;
+        }
+
+        .main-menu .navigation > li.dropdown:hover > a:before,
+        .main-menu .navigation > li.dropdown.current > a:before {
+            color: #FEB605 !important;
+        }
+
+        /* Kustomisasi Mobile Menu Navbar */
+        .mobile-menu .navigation li > a {
+            color: #0052B5 !important;
+        }
+        .mobile-menu .navigation li > a:hover,
+        .mobile-menu .navigation li.current > a {
+            color: #FEB605 !important;
+        }
+
+        /* Kustomisasi Button Kontak */
+        .btn-kontak {
+            background-color: #0052B5 !important;
+            color: #ffffff !important;
+            border-radius: 6px !important;
+            padding: 8px 24px !important;
+            font-weight: 600 !important;
+            transition: all 0.3s ease !important;
+            display: inline-block;
+        }
+        .btn-kontak:hover {
+            background-color: #FEB605 !important;
+            color: #ffffff !important;
+        }
     </style>
 </head>
 
@@ -81,8 +125,9 @@
 
                         <!-- Logo Atas Kiri -->
                         <div class="logo-box clearfix">
-                            <div class="logo" id="logo_webadmin_jaksel">
-                                <img src="<?= base_url('public/assets/images/logo/jaksel-hijau.png') ?>">
+                            <div class="logo d-flex align-items-center" id="logo_webadmin_jaksel">
+                                <img src="<?= base_url('public/assets/images/logo/jaksel-hijau.png') ?>" alt="Logo Jakarta Selatan" style="max-height: 50px; margin-right: 15px;">
+                                <img src="<?= base_url('upload/seeder/logo-diskominfotik.jpeg') ?>" alt="Logo Diskominfotik" style="max-height: 50px;">
                             </div>
                             <div class="search-btn search-btn-one" style="padding-left: 80px;"></div>
                         </div>
@@ -97,39 +142,31 @@
                                 <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
                                     <ul class="navigation clearfix">
                                         <li class="<?= $uri->getSegment(1) == '' ? 'current' : '' ?>"><a href="<?= base_url("/") ?>" id="beranda">Beranda</a></li>
-                                        <li class="<?= $uri->getSegment(1) == 'profil' ? 'current' : '' ?>"><a href="<?= base_url("profil") ?>" id="profil">Profil Sudinsos</a></li>
-                                        <li class="<?= $uri->getSegment(1) == 'pelayanan' ? 'current' : '' ?>"><a href="<?= base_url("pelayanan") ?>" id="pelayanan">Pelayanan</a></li>
-                                        <li class="<?= $uri->getSegment(1) == 'berita' ? 'current' : '' ?>"><a href="<?= base_url("berita") ?>" id="berita">Berita</a></li>
+                                        <li class="dropdown <?= $uri->getSegment(1) == 'profil' ? 'current' : '' ?>"><a href="<?= base_url("profil") ?>" id="profil">Profil</a>
+                                            <ul>
+                                                <li><a href="<?= base_url("profil/struktur-organisasi") ?>">Struktur Organisasi</a></li>
+                                                <li><a href="<?= base_url("profil/tugas-fungsi") ?>">Tugas dan Fungsi</a></li>
+                                                <li><a href="<?= base_url("profil/kinerja") ?>">Kinerja</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown <?= $uri->getSegment(1) == 'seksi' ? 'current' : '' ?>"><a href="<?= base_url("seksi") ?>" id="seksi">Seksi</a>
+                                            <ul>
+                                                <li><a href="<?= base_url("seksi/tata-usaha") ?>">Tata Usaha</a></li>
+                                                <li><a href="<?= base_url("seksi/komunikasi-informasi-publik") ?>">Komunikasi dan Informasi Publik</a></li>
+                                                <li><a href="<?= base_url("seksi/aplikasi-siber-statistik") ?>">Aplikasi, Siber dan Statistik</a></li>
+                                                <li><a href="<?= base_url("seksi/infrastruktur-digital") ?>">Infrastruktur Digital</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="<?= $uri->getSegment(1) == 'pjlp' ? 'current' : '' ?>"><a href="<?= base_url("pjlp") ?>" id="pjlp">PJLP</a></li>
                                     </ul>
                                 </div>
                             </nav>
                         </div>
 
-                        <!-- Social Links -->
-                        <div class="other-links clearfix" style="margin-right: 20px;">
-                            <div class="social-links-one">
-                                <ul class="clearfix">
-                                    <li><a href="https://facebook.com/PemkotJakartaSelatan" target="_blank" class="has-tooltip"><span class="fab fa-facebook-f"></span>
-                                            <div class="c-tooltip">
-                                                <div class="tooltip-inner">Facebook</div>
-                                            </div>
-                                        </a></li>
-                                    <li><a href="https://twitter.com/kotajaksel" target="_blank" class="has-tooltip img-twitter-x"><span class="img-twitter-x"></span>
-                                            <div class="c-tooltip">
-                                                <div class="tooltip-inner">Twitter</div>
-                                            </div>
-                                        </a></li>
-                                    <li><a href="https://www.youtube.com/KominfotikJaksel" target="_blank" class="has-tooltip"><span class="fab fa-youtube"></span>
-                                            <div class="c-tooltip">
-                                                <div class="tooltip-inner">Youtube</div>
-                                            </div>
-                                        </a></li>
-                                    <li><a href="https://www.instagram.com/kotajakartaselatan/" target="_blank" class="has-tooltip"><span class="fab fa-instagram"></span>
-                                            <div class="c-tooltip">
-                                                <div class="tooltip-inner">Instagram</div>
-                                            </div>
-                                        </a></li>
-                                </ul>
+                        <!-- Contact Button -->
+                        <div class="other-links clearfix d-flex align-items-center" style="margin-right: 20px; height: 100px;">
+                            <div class="link-box">
+                                <a href="<?= base_url('kontak') ?>" class="theme-btn btn-kontak"><span class="txt">Kontak</span></a>
                             </div>
                         </div>
 
@@ -143,7 +180,10 @@
                 <div class="close-btn"><span class="icon flaticon-targeting-cross"></span></div>
 
                 <nav class="menu-box">
-                    <div class="nav-logo"><a href="<?= base_url('/') ?>"><img src="<?= base_url('public/assets/images/logo/jaksel-putih.png') ?>"></a></div>
+                    <div class="nav-logo d-flex align-items-center">
+                        <a href="<?= base_url('/') ?>"><img src="<?= base_url('public/assets/images/logo/jaksel-hijau.png') ?>" alt="Logo Jakarta Selatan" style="max-height: 50px; margin-right: 10px;"></a>
+                        <a href="<?= base_url('/') ?>"><img src="<?= base_url('upload/seeder/logo-diskominfotik.jpeg') ?>" alt="Logo Diskominfotik" style="max-height: 50px;"></a>
+                    </div>
                     <div class="menu-outer">
                         <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
                     </div>
